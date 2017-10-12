0dbc45267fae3a82e625c61209bee4e7ed54ce80
Boogie <Boogie@BOOGIE-PC>
2017-10-12 16:15:59 -0400

    Created Master Test

---
+++ UnitTestProject1/MasterTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    public class MasterTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
