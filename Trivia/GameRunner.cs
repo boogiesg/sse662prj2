f8921c090f47e01b59569af6b4b4f1da4aeb2fbc
Boogie <Boogie@BOOGIE-PC>
2017-10-12 16:39:39 -0400

    Changed Main program to split away seed generation.  Created Run method that takes in a Random seed.

--- Trivia/GameRunner.cs
+++ Trivia/GameRunner.cs
@@ -12,15 +12,26 @@ namespace UglyTrivia
 
         private static bool notAWinner;
 
+        /*
+         * I needed to split the seed generation away from the code that runs the game
+         * This allows running the game multiple times with the same random seed.
+         * This was preventing the program from being deterministic.
+         * */
         public static void Main(String[] args)
+        {
+            Random rand = new Random();
+            Run(rand);
+        }
+
+        public static void Run(Random rand)
         {
             Game aGame = new Game();
 
             aGame.add("Chet");
             aGame.add("Pat");
             aGame.add("Sue");
-
-            Random rand = new Random();
+        
+            //Random rand = new Random();
 
             do
             {
